187de1cd2ce923d5000650eaebb4b7b31e399501
marm92 <ingramirezmolina2020@gmail.com>
2023-05-17 09:16:40 -0600

    First Commit - CSS breaks app

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+
+    const [ categories, setCategories ] = useState([ 'One Punch']);
+    
+    
+    const onAddCategory = (newCategory) => {
+
+        if ( categories.includes( newCategory) ) return; // para evitar repetidos
+        //console.log(newCategory);
+        //categories.push(newCategory);
+        setCategories([ newCategory, ...categories ]);
+        //console.log('valorant')
+    };
+    
+
+  return (
+    <>
+        <h1>GifExpertApp</h1>
+        
+        
+        <AddCategory 
+            //setCategories={ setCategories } 
+            onNewCategory={ event => onAddCategory(event) }
+        />
+
+        
+        { 
+            categories.map( (category) => (
+                    <GifGrid 
+                        key={ category } 
+                        category={ category }
+                    />
+                )
+            ) 
+        }
+        
+            { /* Gif */ }
+  
+    </>
+  )
+}
